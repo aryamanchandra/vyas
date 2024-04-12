003d575dcdf4aad5c55b129d14d870a1c2636ac8
aryamanchandra <aryamanhere@gmail.com>
2024-04-13 02:17:25 +0530

    first commit

---
+++ src/screens/You.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const You = () => {
+  return (
+    <View>
+      <Text>You</Text>
+    </View>
+  )
+}
+
+export default You
+
+const styles = StyleSheet.create({})
